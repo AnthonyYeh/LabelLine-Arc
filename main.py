cbf6cf87786f9317e573f5a3b6b051271f741ea4
AnthonyYeh <106139042+AnthonyYeh@users.noreply.github.com>
2022-10-31 22:47:22 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,107 @@
+
 import pygame
 import cv2
 import imutils
 import numpy as np
+
+
 filename="stylized.jpg"
 x_size=800
 y_size=600
 image=cv2.imread(filename)
 image=cv2.resize(image, (x_size, y_size))
 image=cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+data=""
+lines=[]
+
+
+def convert_arc(pt1, pt2, sagitta):
+    # extract point coordinates
+    x1, y1 = pt1
+    x2, y2 = pt2
+
+    # find normal from midpoint, follow by length sagitta
+    n = np.array([y2 - y1, x1 - x2])
+    n_dist = np.sqrt(np.sum(n ** 2))
+
+    if np.isclose(n_dist, 0):
+        # catch error here, d(pt1, pt2) ~ 0
+        print('Error: The distance between pt1 and pt2 is too small.')
+
+    n = n / n_dist
+    x3, y3 = (np.array(pt1) + np.array(pt2)) / 2 + sagitta * n
+
+    # calculate the circle from three points
+    # see https://math.stackexchange.com/a/1460096/246399
+    A = np.array([
+        [x1 ** 2 + y1 ** 2, x1, y1, 1],
+        [x2 ** 2 + y2 ** 2, x2, y2, 1],
+        [x3 ** 2 + y3 ** 2, x3, y3, 1]])
+    M11 = np.linalg.det(A[:, (1, 2, 3)])
+    M12 = np.linalg.det(A[:, (0, 2, 3)])
+    M13 = np.linalg.det(A[:, (0, 1, 3)])
+    M14 = np.linalg.det(A[:, (0, 1, 2)])
+
+    if np.isclose(M11, 0):
+        # catch error here, the points are collinear (sagitta ~ 0)
+        print('Error: The third point is collinear.')
+
+    cx = 0.5 * M12 / M11
+    cy = -0.5 * M13 / M11
+    radius = np.sqrt(cx ** 2 + cy ** 2 + M14 / M11)
+
+    # calculate angles of pt1 and pt2 from center of circle
+    pt1_angle = 180 * np.arctan2(y1 - cy, x1 - cx) / np.pi
+    pt2_angle = 180 * np.arctan2(y2 - cy, x2 - cx) / np.pi
+
+    return (cx, cy), radius, pt1_angle, pt2_angle
+
+
+def draw_ellipse(
+        img, center, axes, angle,
+        startAngle, endAngle, color,
+        thickness=1, lineType=cv2.LINE_AA, shift=10):
+    # uses the shift to accurately get sub-pixel resolution for arc
+    # taken from https://stackoverflow.com/a/44892317/5087436
+    center = (
+        int(round(center[0] * 2 ** shift)),
+        int(round(center[1] * 2 ** shift))
+    )
+    axes = (
+        int(round(axes[0] * 2 ** shift)),
+        int(round(axes[1] * 2 ** shift))
+    )
+    cv2.ellipse(
+        img, center, axes, angle,
+        startAngle, endAngle, color,
+        thickness, lineType, shift)
+    return img
+
+
+def draw(img, p1, p2, sag, thick, colour):
+    if sag == 0:
+        img = cv2.line(img, p1, p2, color=colour, thickness=thick)
+        return img
+    else:
+
+        center, radius, start_angle, end_angle = convert_arc(p1, p2, sag)
+        axes = (radius, radius)
+        img = draw_ellipse(img, center, axes, 0, start_angle, end_angle, color=colour, thickness=thick)
+
+        return img
+with open("data.txt", 'r') as op:
+    data=op.read()
+data=data.split('\n')
+lines=[eval(i) for i in data]
+for obj in lines:
+    temp_sag = 0
+    pointarray = []
+    for param in obj:
+        if type(param)==list:
+            pointarray=param
+        if type(param)==int:
+            temp_sag=param
+    draw(image, pointarray[0], pointarray[1], temp_sag, 1, (255, 0, 0))
 while True:
     pgimg=imutils.rotate_bound(image, -90)
     gameDisplay = pygame.display.set_mode((x_size,y_size))
@@ -20,80 +114,7 @@ while True:
     temp:np.ndarray
 
 
-    def convert_arc(pt1, pt2, sagitta):
-        # extract point coordinates
-        x1, y1 = pt1
-        x2, y2 = pt2
 
-        # find normal from midpoint, follow by length sagitta
-        n = np.array([y2 - y1, x1 - x2])
-        n_dist = np.sqrt(np.sum(n**2))
-
-        if np.isclose(n_dist, 0):
-            # catch error here, d(pt1, pt2) ~ 0
-            print('Error: The distance between pt1 and pt2 is too small.')
-
-        n = n/n_dist
-        x3, y3 = (np.array(pt1) + np.array(pt2))/2 + sagitta * n
-
-        # calculate the circle from three points
-        # see https://math.stackexchange.com/a/1460096/246399
-        A = np.array([
-            [x1**2 + y1**2, x1, y1, 1],
-            [x2**2 + y2**2, x2, y2, 1],
-            [x3**2 + y3**2, x3, y3, 1]])
-        M11 = np.linalg.det(A[:, (1, 2, 3)])
-        M12 = np.linalg.det(A[:, (0, 2, 3)])
-        M13 = np.linalg.det(A[:, (0, 1, 3)])
-        M14 = np.linalg.det(A[:, (0, 1, 2)])
-
-        if np.isclose(M11, 0):
-            # catch error here, the points are collinear (sagitta ~ 0)
-            print('Error: The third point is collinear.')
-
-        cx = 0.5 * M12/M11
-        cy = -0.5 * M13/M11
-        radius = np.sqrt(cx**2 + cy**2 + M14/M11)
-
-        # calculate angles of pt1 and pt2 from center of circle
-        pt1_angle = 180*np.arctan2(y1 - cy, x1 - cx)/np.pi
-        pt2_angle = 180*np.arctan2(y2 - cy, x2 - cx)/np.pi
-
-        return (cx, cy), radius, pt1_angle, pt2_angle
-
-
-    def draw_ellipse(
-            img, center, axes, angle,
-            startAngle, endAngle, color,
-            thickness=1, lineType=cv2.LINE_AA, shift=10):
-        # uses the shift to accurately get sub-pixel resolution for arc
-        # taken from https://stackoverflow.com/a/44892317/5087436
-        center = (
-            int(round(center[0] * 2**shift)),
-            int(round(center[1] * 2**shift))
-        )
-        axes = (
-            int(round(axes[0] * 2**shift)),
-            int(round(axes[1] * 2**shift))
-        )
-        cv2.ellipse(
-            img, center, axes, angle,
-            startAngle, endAngle, color,
-            thickness, lineType, shift)
-        return img
-
-
-    def draw(img, p1, p2, sag, thick, colour):
-        if sag==0:
-            img=cv2.line(img, p1, p2, color=colour, thickness=thick)
-            return img
-        else:
-
-            center, radius, start_angle, end_angle = convert_arc(p1, p2, sag)
-            axes = (radius, radius)
-            img = draw_ellipse(img, center, axes, 0, start_angle, end_angle, color=colour, thickness=thick)
-
-            return img
 
 
     while run:
